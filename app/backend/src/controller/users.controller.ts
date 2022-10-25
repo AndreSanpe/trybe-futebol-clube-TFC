c52a3819443f4b68a4a35db3c639234b1f524446
André Santos <x_sode@hotmail.com>
2022-10-25 10:24:47 -0300

    'feat: user controller file'

---
+++ app/backend/src/controller/users.controller.ts
@@ -0,0 +1,20 @@
+// import { Request, Response } from 'express';
+// // import { StatusCodes } from './statusCodes';
+// import UserService from '../services/users.service';
+
+// class UsersController {
+//   constructor(private userService = new UserService()) { }
+
+//   public addUser = async (req: Request, res: Response) => {
+//     const item = req.body;
+//     const user = await this.userService.addUser(item);
+//     res.status(201).json({ token: user });
+//   };
+
+//   // public getAllProducts = async (_req: Request, res: Response) => {
+//   //   const products = await this.productService.getAllProducts();
+//   //   res.status(200).json(products);
+//   // };
+// }
+
+// export default UsersController;
