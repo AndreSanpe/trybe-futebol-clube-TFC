5494c533092342463a2a0841151bf07c76973fd6
André Santos <x_sode@hotmail.com>
2022-10-25 10:27:40 -0300

    'test: TDD test of login route and / route'

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,53 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import UserModel from '../database/models/Users';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testing login route', () => {
+
+    let chaiHttpResponse: Response;
+
+//   beforeEach(async () => {
+//     sinon
+//       .stub(UserModel, "findOne")
+//       .resolves({
+//         ...<Seu mock>
+//       } as UserModel);
+//   });
+
+  // after(()=>{
+  //   (Example.findOne as sinon.SinonStub).restore();
+  // })
+  describe('First we are going to test the "/" route', () => {
+      it('testing if the we can access the route /', async () => {
+        chaiHttpResponse = await chai.request(app).get('/')
+        console.log(chaiHttpResponse);
+        
+        expect(chaiHttpResponse).to.equal(201);
+      });
+  
+  })
+  describe('test the "/login" route', () => {
+    it('testing if the we can access the route "/login"', async () => {
+      chaiHttpResponse = await chai.request(app).post('/login')
+      expect(chaiHttpResponse).to.equal(201);
+    });
+
+})
+});
+
+
+
+
+
+
+
