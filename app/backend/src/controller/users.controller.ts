036d8ccf22897dcde5e3bb8226d639006fcd0d9b
André Santos <x_sode@hotmail.com>
2022-10-25 21:47:52 -0300

    'feat: using try and cat in the user controller'

--- app/backend/src/controller/users.controller.ts
+++ app/backend/src/controller/users.controller.ts
@@ -1,13 +1,16 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import UserService from '../services/users.service';
 
 class UsersController {
   constructor(private userService = new UserService()) { }
 
-  public getUser = async (req: Request, res: Response) => {
-    // const { password, email } = req.body;
-    // const user = await this.userService.login(email, password);
-    res.sendStatus(201);
+  public getUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const user = await this.userService.login(req.body);
+      res.status(201).json({ user });
+    } catch (error) {
+      next(error);
+    }
   };
 }
 
