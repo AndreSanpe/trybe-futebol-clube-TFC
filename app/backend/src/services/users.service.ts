1722586e73e3c03bdfe6f88a9521fa9621b209eb
André Santos <x_sode@hotmail.com>
2022-10-25 21:49:20 -0300

    'feat: implemented login method on server layer'

--- app/backend/src/services/users.service.ts
+++ app/backend/src/services/users.service.ts
@@ -1,10 +1,13 @@
+import IUser from '../interfaces/IUser';
 import Users from '../database/models/Users';
 
 class LoginService {
-  private model = Users;
-  public async login(email: string, password: string) {
-    await this.model.findOne({ where: { email, password } });
-  }
+  // private model = Users;
+  // this implementation was suggested by Junior.
+  public login = async ({ email }: IUser) => {
+    const user = await Users.findOne({ where: { email } });
+    return user;
+  };
 }
 
 export default LoginService;
