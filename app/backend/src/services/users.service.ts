8072cfdec0cf686d5a736595a49c1a8d791e0fd6
André Santos <x_sode@hotmail.com>
2022-10-26 16:38:17 -0300

    'feat: validate servece'

--- app/backend/src/services/users.service.ts
+++ app/backend/src/services/users.service.ts
@@ -1,25 +1,34 @@
 import * as bcrypt from 'bcryptjs';
-import { ILogin } from '../interfaces/IUser';
+import { ILogin, IUserRole } from '../interfaces/IUser';
 import User from '../database/models/User';
 import HttpError from '../utils/HttpError';
 import TokerManager from '../utils/jwt.util';
 
+const errorMessage = 'Incorrect email or password';
+
 class LoginService {
   // private model = User;
   // this implementation was suggested by Junior.
+
   public login = async ({ email, password: passwordUser }: ILogin) => {
     const user = await User.findOne({ where: { email } });
 
-    if (!user) { throw new HttpError(401, 'Incorrect email or password'); }
+    if (!user) { throw new HttpError(401, errorMessage); }
 
     const { password, role } = user;
     const passBcrypto = await bcrypt.compare(passwordUser, password);
 
-    if (!passBcrypto) { throw new HttpError(401, 'Incorrect email or password'); }
+    if (!passBcrypto) { throw new HttpError(401, errorMessage); }
 
     const token = TokerManager.create({ email, role });
 
     return token;
+  };
+
+  public validate = (token: string) => {
+    const userRole = TokerManager.verify(token);
+
+    return userRole;
   };
 }
 
