ead16f64f8e3e6b0429053f30b476dd2fc6c31bb
André Santos <x_sode@hotmail.com>
2022-10-26 15:28:51 -0300

    'fix: changing message in schema joi'

--- app/backend/src/middleware/schemas/login.ts
+++ app/backend/src/middleware/schemas/login.ts
@@ -1,8 +1,18 @@
-import * as Joi from 'joi';
+import Joi from 'joi';
+
+const errorMessage = 'All fields must be filled';
 
 const login = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
+  email: Joi.string().email().required().messages({
+    'any.required': errorMessage,
+    'string.email': 'Incorrect email or password',
+    'string.empty': errorMessage,
+  }),
+  password: Joi.string().required().messages({
+    'any.required': errorMessage,
+    'string.email': 'Incorrect email or password',
+    'string.empty': errorMessage,
+  }),
 });
 
 export default login;
