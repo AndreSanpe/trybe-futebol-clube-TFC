cc5ec10a0e751639ea0bbb142aff00edf255869a
André Santos <x_sode@hotmail.com>
2022-10-26 16:37:36 -0300

    'feat: insert validate route'

--- app/backend/src/routes/user.route.ts
+++ app/backend/src/routes/user.route.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import loginMid from '../middleware/loginMid';
-import { UserController } from '../controller';
+import { UserController, ValidateController } from '../controller';
 
 const router = Router();
 
 const useController = new UserController();
+const validateController = new ValidateController();
 
 router.post('/login', loginMid, useController.getUser);
 
-// router.get('/login/validate', ValidateController.getUser);
+router.get('/login/validate', validateController.validateUser);
 
 export default router;
