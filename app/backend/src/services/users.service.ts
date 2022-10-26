dcc35b0a41407cd6062f3e5dd54fa1d1adde7b07
André Santos <x_sode@hotmail.com>
2022-10-25 22:40:39 -0300

    'feat: using bcrypt to validate the signature'

--- app/backend/src/services/users.service.ts
+++ app/backend/src/services/users.service.ts
@@ -1,3 +1,4 @@
+import * as bcrypt from 'bcryptjs';
 import IUser from '../interfaces/IUser';
 import Users from '../database/models/Users';
 import HttpError from '../utils/HttpError';
@@ -6,10 +7,12 @@ import TokerManager from '../utils/jwt.util';
 class LoginService {
   // private model = Users;
   // this implementation was suggested by Junior.
-  public login = async ({ email, password }: IUser) => {
-    const user = await Users.findOne({ where: { email } });
+  public login = async ({ email, password: passwordUser }: IUser) => {
+    const user:any = await Users.findOne({ where: { email } });
+    const { password } = user;
+    const passBcrypto = await bcrypt.compare(passwordUser, password);
 
-    if (!user) { throw new HttpError(401, 'Username or password invalid'); }
+    if (!passBcrypto) { throw new HttpError(401, 'Username or password invalid'); }
 
     const token = TokerManager.create({ email, password });
 
