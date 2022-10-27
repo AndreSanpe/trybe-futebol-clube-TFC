771f8b1a17de5c8a8925f2421ffb68730e1ee91d
André Santos <x_sode@hotmail.com>
2022-10-27 15:46:04 -0300

    'feat: validate token middleware'

---
+++ app/backend/src/middleware/validateToken.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+import TokerManager from '../utils/jwt.util';
+
+const validateToken = (req:Request, res:Response, next:NextFunction) => {
+  const { authorization } = req.headers;
+  console.log(authorization);
+
+  if (!authorization) {
+    return res.status(401).json({ message: 'Invalid authorization' });
+  }
+
+  const isItValid = TokerManager.verify(authorization);
+  if (!isItValid) {
+    return res.status(401).json({ message: 'Invalid authorization' });
+  }
+
+  next();
+};
+
+export default validateToken;
