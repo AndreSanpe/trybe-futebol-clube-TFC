b346629b5092eb8cd91ac2629452c191f0b1501c
André Santos <x_sode@hotmail.com>
2022-10-31 15:58:25 -0300

    'fix: fixing query'

--- app/backend/src/utils/queries.ts
+++ app/backend/src/utils/queries.ts
@@ -118,7 +118,7 @@ ORDER BY totalPoints DESC, goalsBalance DESC, totalVictories DESC, goalsFavor DE
 const queryBoard = `SELECT 
 
 a.name as name, 
-a.totalPoints + b.totalPoints AS totalPoints,
+(a.totalPoints + b.totalPoints) AS totalPoints,
 a.totalGames + b.totalGames AS totalGames,
 a.totalVictories + b.totalVictories AS totalVictories,
 a.totalDraws + b.totalDraws AS totalDraws,
@@ -126,10 +126,9 @@ a.totalLosses + b.totalLosses AS totalLosses,
 a.goalsFavor + b.goalsFavor AS goalsFavor,
 a.goalsOwn + b.goalsOwn AS goalsOwn,
 a.goalsBalance + b.goalsBalance AS goalsBalance,
-ROUND((a.totalPoints + b.totalPoints)/
-((a.totalGames + b.totalGames)*3)*100,2) AS efficiency
+ROUND((a.totalPoints + b.totalPoints)/((a.totalGames + b.totalGames)*3)*100,2) AS efficiency
 
-from (SELECT team_name as name, 
+FROM (SELECT team_name as name, 
 
 (SELECT SUM(CASE 
 WHEN (home_team_goals > away_team_goals) THEN 3
@@ -167,24 +166,16 @@ SUM(away_team_goals) as goalsOwn,
 (SELECT SUM( 
 SUM(home_team_goals)-SUM(away_team_goals)
 )) 
-as goalsBalance,
-            
-((ROUND
-(((SELECT SUM(CASE 
-WHEN (home_team_goals < away_team_goals) THEN 0
-WHEN (home_team_goals > away_team_goals) THEN 3
-ELSE 1 
-            END))
-            /
-            ((SELECT COUNT(home_team)*3)))*100,2))
-            
-            ) as efficiency
-            
-            
+as goalsBalance
+
 FROM TRYBE_FUTEBOL_CLUBE.matches as m
 LEFT JOIN TRYBE_FUTEBOL_CLUBE.teams as t ON m.home_team = t.id
 
-GROUP BY t.id) as a
+WHERE in_progress = 0
+
+GROUP BY t.id)
+ as a
+
 INNER JOIN 
 (SELECT team_name as name, 
 
@@ -224,25 +215,16 @@ SUM(home_team_goals) as goalsOwn,
 (SELECT SUM( 
 SUM(away_team_goals)-SUM(home_team_goals)
 )) 
-as goalsBalance,
-            
-((ROUND
-(((SELECT SUM(CASE 
-WHEN (away_team_goals < home_team_goals) THEN 0
-WHEN (away_team_goals > home_team_goals) THEN 3
-ELSE 1 
-            END))
-            /
-            ((SELECT COUNT(away_team)*3)))*100,2))
-            
-            ) as efficiency
-            
+as goalsBalance
             
 FROM TRYBE_FUTEBOL_CLUBE.matches as m
 LEFT JOIN TRYBE_FUTEBOL_CLUBE.teams as t ON m.away_team = t.id WHERE in_progress = 0
 
-GROUP BY t.id) as b
-on a.name = b.name
-ORDER BY totalPoints DESC, goalsBalance DESC, totalVictories DESC, goalsFavor DESC, goalsOwn DESC;`;
+GROUP BY t.id
+) as b
+on b.name = a.name
+ORDER BY totalPoints DESC, goalsBalance DESC, totalVictories DESC, goalsFavor DESC, goalsOwn DESC;
+
+`;
 
 export { queryBoardHome, queryBoardAway, queryBoard };
